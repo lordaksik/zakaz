a5a42cb8cbbd01f8cd173fc52ce1fe75d9779ca9
lordaksik <48729120+lordaksik@users.noreply.github.com>
2021-12-26 12:17:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,18 +11,25 @@ bot.hears('/bot', async (ctx) => {
         const response = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/7/1/")
         const data = await response.json()
        let arr=[];
-        for(let i=0; i<5;i++){
-     
-        colors =data.items.results[i].results[0].color
-       arr.push(colors);
-    }
-   
-       ctx.reply("Ставим:  ⬜Серый ⬛ Чёрный");
-       console.log("Ставим:  ⬜Серый ⬛ Чёрный");   
-    }
+     for (let i = 0; i <= 10; i++) {
+
+         colors = data.items.results[i].results[0].color
+
+         arr.push(colors);
+      }
+      if (arr.includes('grey') = false) {
+         ctx.reply("Серого цвета не было 10 раз");
+      }
+      if (arr.includes('black') = false) {
+         ctx.reply("Чёрного цвета не было 10 раз");
+      }
+
+      if (arr.includes('red') = false) {
+         ctx.reply("Чёрного цвета не было 10 раз");
+      }
     function good(){
         ctx.reply('Бот запущен!')
-       global.time= setInterval(request2,10000) 
+       global.time= setInterval(request2,60000) 
        }
 
 
