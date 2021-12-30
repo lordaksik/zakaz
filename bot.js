f71e8e53adc3967f5994af76eef6d19f0142238d
lordaksik <48729120+lordaksik@users.noreply.github.com>
2021-12-30 13:51:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,14 +27,14 @@ bot.hears('/bot', async (ctx) => {
          arr.push(colors);
       }
       if (arr.includes('grey') === false) {
-         ctx.reply("Серого цвета не было 18 раз");
+         ctx.reply("Серого цвета не было 10 раз");
       }
       if (arr.includes('black') === false) {
-         ctx.reply("Чёрного цвета не было 18 раз");
+         ctx.reply("Чёрного цвета не было 10 раз");
       }
 
       if (arr.includes('red') === false) {
-         ctx.reply("Чёрного цвета не было 18 раз");
+         ctx.reply("Чёрного цвета не было 10 раз");
       }
 
 
