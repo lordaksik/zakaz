215c321f7793c7477f3b4cf0ebb51090d11d3bb4
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-01-09 21:54:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,21 +20,21 @@ bot.hears('/bot', async (ctx) => {
       const data = await response.json()
       let arr = [];
       let arrs = [];
-      for (let i = 0; i <= 10; i++) {
+      for (let i = 0; i <= 14; i++) {
 
          colors = data.items.results[i].results[0].color
 
          arr.push(colors);
       }
       if (arr.includes('grey') === false) {
-         ctx.reply("Серого цвета не было 10 раз");
+         ctx.reply("Серого цвета не было 14 раз");
       }
       if (arr.includes('black') === false) {
-         ctx.reply("Чёрного цвета не было 10 раз");
+         ctx.reply("Чёрного цвета не было 14 раз");
       }
 
       if (arr.includes('red') === false) {
-         ctx.reply("Красного цвета не было 10 раз");
+         ctx.reply("Красного цвета не было 14 раз");
       }
 
 
@@ -48,7 +48,7 @@ bot.hears('/bot', async (ctx) => {
       }
 
       if (global.firstch === 0) {
-         ctx.reply("Кубка не было 30 раз");
+         ctx.reply("Кубка не было 60 раз");
       }
 
 
