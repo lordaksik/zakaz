7d5663ec9119a61bf4bd33af478e4260db0d15e8
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-04-03 03:58:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,16 +15,37 @@ bot.hears('/bot', async (ctx) => {
       const data = await response.json()
       let arr = [];
       let num=0;
+      let number=0;
       let num2=0;
       let arrs = [];
       let arrs2 = [];
       let kol=9;
+      let kol175,kol200;
+      let kol100,kol125;
       for (let i = 0; i <= 0; i++) {
-         let number = parseInt(data.items.results[i].results[0].number,10)+parseInt(data.items.results[i].results[1].number,10)
+         number = parseInt(data.items.results[i].results[0].number,10)+parseInt(data.items.results[i].results[1].number,10)
+         +parseInt(data.items.results[i].results[2].number,10)+parseInt(data.items.results[i].results[3].number,10)
+         +parseInt(data.items.results[i].results[4].number,10)+parseInt(data.items.results[i].results[5].number,10)
+         +parseInt(data.items.results[i].results[6].number,10)
         
-         ctx.reply(number);
       }
-       
+       ctx.reply(number);
+      if(number>175)
+      {
+      ctx.reply('число больше 175 не было 20 раз');
+      }
+       if(number>200)
+      {
+      ctx.reply('число больше 200 не было 20 раз');
+      }
+        if(number<125)
+      {
+      ctx.reply('число меньше 125 не было 20 раз');
+      }
+       if(number<100)
+      {
+      ctx.reply('число меньше 100 не было 20 раз');
+      }
 
    }
    function good() {
